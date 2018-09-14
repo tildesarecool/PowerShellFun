4ba9ec7e8355f36e84a8ddbb0185986c1e83f2cf
tildes <tildesare.cool@gmail.com>
2018-09-13 19:47:01 -0700

    slight number of comments

--- helloworld.ps1
+++ helloworld.ps1
@@ -1,1 +1,7 @@
+# apparently this is comment symbol for powershell
+# wanted to go this powershell book back in july 
+# but never got to it so here i am doing it in september
+# chapter 1 was pretty worthless
+# 
 $PSVersionTable
+
